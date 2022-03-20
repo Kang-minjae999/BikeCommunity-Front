55bd7626b38fdadfdbb50cfca1918e37c37de55b
Kang-minjae999 <96714650+Kang-minjae999@users.noreply.github.com>
2022-03-20 13:21:41 +0900

    ㅁㄴㅇㄴㅁ
    ㄴㅁㅇㅁㄴㅇ

--- src/pages/dashboard/Blogdingstas.js
+++ src/pages/dashboard/Blogdingstas.js
@@ -79,6 +79,7 @@ export default function Blogdingstas() {
     }
   };
 
+
   const handleChange = useCallback(
     (event, value) => {
       setpagenation(value);
@@ -115,7 +116,7 @@ export default function Blogdingstas() {
         <Grid container spacing={3}>
           {(!posts.length ? [...Array(12)] : sortedPosts).map((post, index) =>
             post ? (
-              <Grid key={post.id} item xs={12} sm={6} md={(index === 0 && 6) || 3}>
+              <Grid key={post.id} item xs={12} sm={6} md={3}>
                 <BlogPostCard post={post} />
               </Grid>
             ) : (
